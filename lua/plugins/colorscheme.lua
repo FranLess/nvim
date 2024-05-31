a41b1bd0c23eafad502f78902bd2698096b96d76
Francisco Martínez <fmart@192.168.1.9>
2024-05-30 18:40:42 -0600

    Initial config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,27 @@
+return {
+  -- add gruvbox
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      -- Enable transparent background
+      transparent = true,
+
+      -- Enable italic comment
+      italic_comments = true,
+
+      -- Underline `@markup.link.*` variants
+      underline_links = true,
+
+      -- Disable nvim-tree background color
+      disable_nvimtree_bg = true,
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
